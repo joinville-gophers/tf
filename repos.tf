a65addb084aaef877246d64e2d5c0f925ab694ca
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-01-24 23:22:56 -0200

    discussions repo

--- repos.tf
+++ repos.tf
@@ -30,3 +30,13 @@ resource "github_repository" "cfp" {
   has_issues   = true
   has_wiki     = false
 }
+
+resource "github_repository" "discussions" {
+  name        = "discussions"
+  description = "Discussions about anything related to our community"
+
+  homepage_url = "https://www.meetup.com/Joinville-Go-Meetup/"
+  has_issues   = true
+  has_wiki     = false
+  auto_init    = true
+}
